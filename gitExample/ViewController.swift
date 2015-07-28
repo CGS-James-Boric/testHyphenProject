ecdd1079b22146a3b2a8984b97ee5a1214cb46ac
James Boric <17765@cgs.act.edu.au>
2015-07-28 17:12:32 +1000

    Added an awesome user feature (x)

--- gitExample/ViewController.swift
+++ gitExample/ViewController.swift
@@ -19,10 +19,11 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+		// Dispose of any resources that can be recreated.
     }
     
-
-
+    func featX() {
+        println("This is an awesome feature")
+    }
 }
 
