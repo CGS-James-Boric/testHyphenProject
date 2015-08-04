938fe573961f8fa23e24546091a0f642c274c1d2
James Boric <17765@cgs.act.edu.au>
2015-08-04 12:41:16 +1000

    changed featX

--- gitExample/ViewController.swift
+++ gitExample/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
     }
     
-    func featX() {
+    func featQ() {
         println("This is an awesome feature")
     }
     
